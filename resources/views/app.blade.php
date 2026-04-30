deedb02ffb948d50dc3fd0ff3640fd16f0008d45
Akmlzzz <142098281+Akmlzzz@users.noreply.github.com>
2026-04-30 15:03:55 +0700

    chore: update favicon tab browser dengan logo baru

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('img/logo/logo-main-bg.svg') }}">
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
     @routes
     @viteReactRefresh
