7e8b22fcdd1fd99b0dcd53976afd196986cd093d
Akmlzzz <142098281+Akmlzzz@users.noreply.github.com>
2026-04-30 07:26:52 +0700

    perbaikan: mengatasi layar putih dan teks literal @route dengan menggunakan direktif standar Inertia dan generate rute Ziggy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,15 +1,17 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    @routes
     @viteReactRefresh
     @vite('resources/js/app.jsx')
-    <x-inertia::head />
+    @inertiaHead
 </head>
-
-<body>
-    <x-inertia::app />
+<body class="font-sans antialiased">
+    @inertia
 </body>
-
 </html>
