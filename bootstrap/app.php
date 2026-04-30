ab0f930b6288d0c8bdfe134487f5096b97d6582f
lion <waedi189@gmail.com>
2026-04-30 14:10:21 +0700

    menambahkan fungsi trust proxies agar ngrok dapat mengakses lokal

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
+        $middleware->trustProxies(at: '*');
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
