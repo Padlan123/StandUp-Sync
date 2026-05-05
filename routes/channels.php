4535e490830bbf50d2189e00945fd51ba228a654
lion <waedi189@gmail.com>
2026-05-05 11:41:20 +0700

    membuat channel broadcast menangani chat

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Models\Group;
+// use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('chat.{group}', function ($user, $group) {
+    $group = Group::find($group->id);
+    if ($group && $group->users()->where('user_id', $user->id)->exists()) {
+        return ['id' => $user->id, 'name' => $user->name, 'role' => $user->getRoleNames()->first()];
+    }
+    return false;
 });
